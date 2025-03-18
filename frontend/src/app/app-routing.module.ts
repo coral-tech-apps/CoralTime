87fc81a7c1836ad4dc3d66808e58f64ecb1d643e
Andrei Lisai <andrei.lisai@coral.team>
2025-03-18 10:22:12 +0300

    Update angular 10 to angular 11
    update angular/core to v11 && angular/cli to v11
    
    update ngx-slimscroll to v11
    
    update ngx-markdown to 11v, ngx-img-cropper to 11

--- frontend/src/app/app-routing.module.ts
+++ frontend/src/app/app-routing.module.ts
@@ -82,7 +82,7 @@ export const appRoutes: Routes = [
 
 @NgModule({
 	imports: [
-		RouterModule.forRoot(appRoutes)
+		RouterModule.forRoot(appRoutes, { relativeLinkResolution: 'legacy' })
 	],
 	exports: [
 		RouterModule
